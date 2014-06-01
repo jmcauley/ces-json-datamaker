66acfa0089d1716b9c211010c81bba6388230794
Judah McAuley <judah.mcauley@gmail.com>
2014-06-01 16:12:44 -0700

    weighting the weights by type of business and type of material

--- parser.js
+++ parser.js
@@ -17,29 +17,54 @@ fs.readFile('./CESsample.json', 'utf8', function (err,data){
     jsonData.business_type = business_type; //set the business type based on args
 
     //this way we can more accurately make test data to play with, based on waste stream estimates from a weighted point, like stddev
-    var business_weight = 0;
+    var readily_recyclable_weight, compostable_weight, other_recoverable_weight, non_recoverable_weight = 0;
     var max_weight = 0;
     var min_weight = 0;
     switch (business_type) {
     case "Retail":
-        business_weight = 1.2;
-        max_weight = 2000;
+        readily_recyclable_weight = 5.0;
+        compostable_weight = 0.8;
+        other_recoverable_weight = 2.0;
+        non_recoverable_weight = 2.0;
+        max_weight = 100;
         min_weight = 0.2;
         break;
-    case "Restaurant":
-        business_weight = 0.7;
-        max_weight = 500;
+    case "Hospitality":
+        readily_recyclable_weight = 1.0;
+        compostable_weight = 1.0;
+        other_recoverable_weight = 1.0;
+        non_recoverable_weight = 1.0;
+        max_weight = 100;
         min_weight = 0.2;
         break;
-    case "Manufacturing":
-        business_weight = 1.4;
-        max_weight = 10000;
+    case "Residential":
+        readily_recyclable_weight = 0.7;
+        compostable_weight = 0.4;
+        other_recoverable_weight = 0.8;
+        non_recoverable_weight = 1.2;
+        max_weight = 100;
+        min_weight = 0.2;
+        break;
+    case "Office":
+        readily_recyclable_weight = 3.0;
+        compostable_weight = 0.8;
+        other_recoverable_weight = 2.0;
+        non_recoverable_weight = 1.0;
+        max_weight = 100;
+        min_weight = 0.2;
+        break;
+    case "Events":
+        readily_recyclable_weight = 4.0;
+        compostable_weight = 1.2;
+        other_recoverable_weight = 0.9;
+        non_recoverable_weight = 2.0;
+        max_weight = 100;
         min_weight = 0.2;
         break;
     }
     //that way we can weight how data is munged in a second
 
-    //bin_length doesnt change through each loop, but contents_lenght does based on the spreadhseet
+    //bin_length doesn't change through each loop, but contents_length does based on the spreadsheet
     var bin_length = jsonData.collection_bins.length
 
     //parse each collection_bin with j index
@@ -47,15 +72,32 @@ fs.readFile('./CESsample.json', 'utf8', function (err,data){
       var contents_length = jsonData.collection_bins[j].contents.length;
       //parse each collection_bin separately
       for (var i = 0; i < contents_length; i++){
-        var line = jsonData.collection_bins[j].contents[i].weight = getRandomInt(min_weight,max_weight);
-        //TODO take into account weights based on section of the jsonData object
-        //if(jsonData.collection_bins[j].name == "Readily Recyclable") something like that
+        var new_weight = 0;
+        switch(jsonData.collection_bins[j].name){
+            case "Readily Recyclable":
+                new_weight = getRandomInt(min_weight,max_weight) * readily_recyclable_weight;
+                break;
+            case "Compostable Materials":
+                new_weight = getRandomInt(min_weight,max_weight) * compostable_weight;
+                break;
+            case "Other Recoverable Materials":
+                new_weight = getRandomInt(min_weight,max_weight) * other_recoverable_weight;
+                break;
+            case "Non-Recoverable Materials":
+                new_weight = getRandomInt(min_weight,max_weight) * non_recoverable_weight;
+                break;
+            default:
+                new_weight = getRandomInt(min_weight,max_weight);
+
+        }
+        jsonData.collection_bins[j].contents[i].weight = new_weight;
+
       }
 
     }
     //end of filling in weight data, now we need to replace the total weight based on this data
     var total_weight;
-    //for looop to add up all the weights
+    //for loop to add up all the weights
     
 
     var grand_total_weight = 0;
@@ -117,7 +159,7 @@ console.log("So you want ", process.argv[2], "new JSON objects to be made.\n", "
 var business_type = process.argv[3];
 var number_new_json = process.argv[2]; //get the amount of new files to make
 
-if(business_type != undefined && number_new_json > 0){ //basic erro checking, could be better
+if(business_type != undefined && number_new_json > 0){ //basic error checking, could be better
 
   //create the new JSON objects
   for (var i = 0; i < number_new_json; i++){ 
